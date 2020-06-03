dd48ef761d2fc620d382cd1a63377c406d138a5c
Dmitry Ulyanov <sillent1987@gmail.com>
2020-06-03 21:50:44 +0300

    one fix builds

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@
 export CGO_ENABLED=0
 export GOOS=linux
 
+go get -u ./cmd/failover/
 go build -ldflags '-extldflags "-static"' -a -v -o bin/linux/failover ./cmd/failover/
--- cmd/failover/main.go
+++ cmd/failover/main.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 func main() {
-	config, err := rest.InCluster()
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
@@ -20,11 +21,11 @@ func main() {
 		panic(err.Error())
 	}
 	for {
-		pods, err := clientset.CoreV1().Services().List(context.TODO, metav1.ListOptions{})
+		pods, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf(pods)
+		fmt.Println(pods)
 
 	}
 
